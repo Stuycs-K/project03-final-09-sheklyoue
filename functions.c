be4e28d5b4bc9e601207968e905fd799becdbd50
Eric You <lyou50@stuy.edu>
2025-01-09 18:10:50 -0500

    cleared extra print statements

--- functions.c
+++ functions.c
@@ -2,33 +2,25 @@
 
 
 int server_setup() {
-    //printf("Setting server up\n");
-    //printf("1. Server Making Pipe\n");
     int from_client;
     if (mkfifo(WKP, 0666) == -1) {
         perror("Named Pipe Error");
         exit(1);
     }
     
-    //printf("2. Server Opening WKP\n");
     from_client = open(WKP, O_RDONLY);
     if (from_client == -1) {
         perror("wkp open error");
         exit(1);
     }
 
-    //printf("4. Server removing WKP\n");
     remove(WKP);
-    //printf("Setting Server up done\n");
-    //printf("from_client = %d\n", from_client);
     return from_client;
 }
 int server_handshake(int *to_client, int from_client) {  
   char client_pipe[BUFFER_SIZE];
   read(from_client, client_pipe, sizeof(client_pipe));
-  //printf("5. Server received SYN: %s\n", client_pipe);
 
-  //printf("6. Server opening Private Pipe\n");
   *(to_client) = open(client_pipe, O_WRONLY);
   if (*(to_client) == -1) {
       perror("open client pipe");
@@ -36,21 +28,17 @@ int server_handshake(int *to_client, int from_client) {
   }
 
   char syn_ack[] = "SYN_ACK";
-  //printf("7. Server sending SYN_ACK\n");
   write(*(to_client), syn_ack, sizeof(syn_ack));
 
   char ack[BUFFER_SIZE];
   read(from_client, ack, sizeof(ack));
-  //printf("9. Server received ACK: %s\n", ack);
 
   printf("Handshake complete\n");
-  //printf("from_client = %d\n", from_client);
   return from_client;
 }
 
 int client_handshake(int *to_server) {
     int from_server;
-    //printf("3. Client creating Private Pipe\n");
     char private_pipe[BUFFER_SIZE];
     snprintf(private_pipe, sizeof(private_pipe), "pipe_%d", getpid());
     if (mkfifo(private_pipe, 0666) == -1) {
@@ -58,7 +46,6 @@ int client_handshake(int *to_server) {
         exit(1);
     }
 
-    //printf("3. Client opening WKP\n");
     *(to_server) = open(WKP, O_WRONLY);
     if (*(to_server) == -1) {
         perror("open WKP");
@@ -67,10 +54,8 @@ int client_handshake(int *to_server) {
     }
     int client_wkp = *(to_server);
 
-    //printf("3. Client writing Private Pipe name to WKP\n");
     write(client_wkp, private_pipe, sizeof(private_pipe));
 
-    //printf("3. Client opening Private Pipe (blocking)\n");
     from_server = open(private_pipe, O_RDONLY);
     if (from_server == -1) {
         perror("open private pipe");
@@ -81,12 +66,9 @@ int client_handshake(int *to_server) {
 
     char syn_ack[BUFFER_SIZE];
     read(from_server, syn_ack, sizeof(syn_ack));
-    //printf("8. Client received SYN_ACK: %s\n", syn_ack);
 
-    //printf("8. Client sending ACK on WKP\n");
     char ack[] = "ACK";
     write(client_wkp, ack, sizeof(ack));
-    //printf("to_server = %d\n", client_wkp);
     return from_server;
 }
 
