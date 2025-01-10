5b504e2f41df2f91ea9697abbb226b2e593be10b
lawrence <lshek50@stuy.edu>
2025-01-09 21:33:32 -0500

    closing server also closes client

--- client.c
+++ client.c
@@ -7,13 +7,19 @@ int from_server;
 void sighandler(int signo) {
   close(to_server);
   close(from_server);
-  printf("\nclient disconnected\n");
+  if (signo == SIGINT) {
+    printf("\nclient disconnected\n");
+  }
+  if (signo == SIGPIPE) {
+    printf("Message not sent because server was closed.\n");
+  }
   exit(0);
 }
 
 
 int main() {
   signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
 
   from_server = client_handshake(&to_server);
 
@@ -25,12 +31,15 @@ int main() {
     // get rid of trailing '\n'
     message[strlen(message) - 1] = '\0';
     // send message to server
-    printf("sending message...\n");
-    write(to_server, message, strlen(message) + 1);
+    int bytesWritten = write(to_server, message, strlen(message) + 1);
 
     // read any messages that were sent to the server
     char buffer[BUFFER_SIZE];
     int bytesRead = read(from_server, buffer, BUFFER_SIZE);
+    if (bytesRead == 0) {
+      printf("Server closed.\n");
+      break;
+    }
     if (bytesRead < 0) {
       perror("client couldn't receive message from server");
       exit(1);
