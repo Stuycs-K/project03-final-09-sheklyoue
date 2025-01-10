657df0e2a0ea6a813e7047efe3b3c79305b72c2e
lawrence <lshek50@stuy.edu>
2025-01-10 12:05:02 -0500

    2 client connected to server kind of works but need to fix lot of things

--- server.c
+++ server.c
@@ -8,19 +8,28 @@ void sighandler(int signo) {
     }
 }
 
+
+
+
 int main() {
     int to_client;
     int from_client;
+
+    int to_client2;
+    int from_client2;
+
     signal(SIGINT, sighandler);
     while (1) {
         printf("Waiting for a client ...\n");
         from_client = server_handshake(&to_client);
         printf("Connected to a client!\n");
 
+        printf("allowing another client..\n");
+        from_client2 = server_handshake(&to_client2);
+        printf("connected to another client\n");
         char buffer[BUFFER_SIZE];
         while (1) {
             int bytesRead = read(from_client, buffer, sizeof(buffer));
-            // client disconnects
             if (bytesRead == 0) {
                 printf("a client disconnected\n");
                 break;
@@ -32,8 +41,39 @@ int main() {
             printf("bytes read: %d\n", bytesRead);
             printf("Server received message : '%s'\n", buffer);
             
-            // send to client
+
+            // send to all clients
             int bytesWritten = write(to_client, buffer, strlen(buffer) + 1);
+            if (bytesWritten < 0) {
+                perror("server fail to send message to client");
+                exit(1);
+            }
+            bytesWritten = write(to_client2, buffer, strlen(buffer) + 1);
+            if (bytesWritten < 0) {
+                perror("server fail to send message to client");
+                exit(1);
+            }
+
+            // read from client 2
+            bytesRead = read(from_client2, buffer, sizeof(buffer));
+            if (bytesRead == 0) {
+                printf("a client disconnected\n");
+                break;
+            }
+            if (bytesRead < 0) {
+                perror("server fail to read message from client");
+                exit(1);
+            }
+            printf("bytes read: %d\n", bytesRead);
+            printf("Server received message : '%s'\n", buffer);
+            
+            // send to all clients
+            bytesWritten = write(to_client, buffer, strlen(buffer) + 1);
+            if (bytesWritten < 0) {
+                perror("server fail to send message to client");
+                exit(1);
+            }
+            bytesWritten = write(to_client2, buffer, strlen(buffer) + 1);
             if (bytesWritten < 0) {
                 perror("server fail to send message to client");
                 exit(1);
@@ -41,5 +81,8 @@ int main() {
         }
         close(from_client);
         close(to_client);
+
+        close(from_client2);
+        close(to_client2);
     }
 }
