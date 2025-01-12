3ca397138c62280028463c0177690776c766f39e
lawrence <lshek50@stuy.edu>
2025-01-12 13:05:16 -0500

    updated server_setup for sockets

--- functions.c
+++ functions.c
@@ -2,85 +2,44 @@
 
 
 int server_setup() {
-    int from_client;
-    if (mkfifo(WKP, 0666) == -1) {
-        perror("Named Pipe Error");
-        exit(1);
-    }
-    
-    from_client = open(WKP, O_RDONLY);
-    if (from_client == -1) {
-        perror("wkp open error");
-        exit(1);
-    }
+  struct addrinfo * hints, * results;//results is allocated in getaddrinfo
+  hints = calloc(1,sizeof(struct addrinfo));
+  char* PORT = "9998";
 
-    remove(WKP);
-    return from_client;
-}
-int server_handshake(int *to_client, int from_client) {  
-  char client_pipe[BUFFER_SIZE];
-  read(from_client, client_pipe, sizeof(client_pipe));
+  hints->ai_family = AF_INET; // domain of socket (tpye of address) 
+  hints->ai_socktype = SOCK_STREAM; //TCP socket
+  hints->ai_flags = AI_PASSIVE; //only needed on server
+  getaddrinfo(NULL, PORT, hints, &results);  //NULL is localhost or 127.0.0.1
 
-  *(to_client) = open(client_pipe, O_WRONLY);
-  if (*(to_client) == -1) {
-      perror("open client pipe");
+  //create listen socket
+  int listen_socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+ 
+  // bind listening socket to address 
+  int err = bind(listen_socket, results->ai_addr, results->ai_addrlen);
+  if(err == -1){
+      printf("Error binding: %s",strerror(errno));
       exit(1);
   }
-
-  char syn_ack[] = "SYN_ACK";
-  write(*(to_client), syn_ack, sizeof(syn_ack));
-
-  char ack[BUFFER_SIZE];
-  read(from_client, ack, sizeof(ack));
-
-  printf("Handshake complete\n");
-  return from_client;
+  // listen socket  
+  listen(listen_socket, 3);//3 clients can wait to be processed
+  printf("Listening on port %s\n",PORT);
+  return listen_socket; 
 }
 
-int client_handshake(int *to_server) {
-    int from_server;
-    char private_pipe[BUFFER_SIZE];
-    snprintf(private_pipe, sizeof(private_pipe), "pipe_%d", getpid());
-    if (mkfifo(private_pipe, 0666) == -1) {
-        perror("mkfifo");
-        exit(1);
-    }
 
-    *(to_server) = open(WKP, O_WRONLY);
-    if (*(to_server) == -1) {
-        perror("open WKP");
-        remove(private_pipe);
-        exit(1);
-    }
-    int client_wkp = *(to_server);
 
-    write(client_wkp, private_pipe, sizeof(private_pipe));
-
-    from_server = open(private_pipe, O_RDONLY);
-    if (from_server == -1) {
-        perror("open private pipe");
-        remove(private_pipe);
-        exit(1);
-    }
-    remove(private_pipe);
-
-    char syn_ack[BUFFER_SIZE];
-    read(from_server, syn_ack, sizeof(syn_ack));
-
-    char ack[] = "ACK";
-    write(client_wkp, ack, sizeof(ack));
-    return from_server;
-}
 
 //Create the user with an username for tracking
 void create_user() {
 
 }
 
+
 //Prints out a list of all the current users in the chat
 void view_users() {
 
 }
+
 
 //Prints the chat on the terminal
 void print_chat(char *filename) {
@@ -98,6 +57,7 @@ void print_chat(char *filename) {
     fclose(file);
 }
 
+
 //Clears all text on the terminal
 void clear_terminal() {
     char *args[] = {"clear", NULL};
@@ -107,9 +67,11 @@ void clear_terminal() {
     }
 }
 
+
 void send_public_message() {
     
 }
+
 
 //Creates the chat and allows for the chatting 
 void create_chat() {
