2da3cb418cba884e61db4b1d9bad2e357e1d94c4
lawrence <lshek50@stuy.edu>
2025-01-12 13:52:23 -0500

    multiplee client conenct

--- server.c
+++ server.c
@@ -1,88 +1,59 @@
-#include "functions.h"
-
-void sighandler(int signo) {
-    if (signo == SIGINT) {
-        printf("\nServer shutting down. Removing WKP.\n");
-        remove(WKP);
-        exit(0);
-    }
-}
-
-
-
+#include "functions.h" 
 
 int main() {
-    int to_client;
-    int from_client;
+    int max_descriptor = 0;
+    int server_socket = server_setup(); 
 
-    int to_client2;
-    int from_client2;
-
-    signal(SIGINT, sighandler);
+    fd_set read_fds;
+    
+    FD_ZERO(&read_fds);
+    FD_SET(server_socket, &read_fds);
+    max_descriptor = server_socket; 
+    
     while (1) {
-        printf("Waiting for a client ...\n");
-        from_client = server_handshake(&to_client);
-        printf("Connected to a client!\n");
+        // create a copy since select modifies the set
+        fd_set copy = read_fds;
+        // waits until any number of fds are available
+        int available = select(max_descriptor + 1, &copy, NULL, NULL, NULL);
+        if (available < 0) {
+            perror("select error");
+            exit(1);
+        }
+        // check each fd to see if it has data to read from
+        for (int i = 0; i <= max_descriptor; i++) {
+            if (!FD_ISSET(i, &copy)) {
+                continue;
+            }
+            if (i == server_socket) {
+                // accept new connection 
+                int client_socket = server_connect(server_socket);  
+                printf("new client connected\n");
+                // add socket to fdset
+                FD_SET(client_socket, &read_fds);
+                // update max_descriptor 
+                if (client_socket > max_descriptor) {
+                    max_descriptor = client_socket; 
+                }
+            }
+            else {
+                // receive message from a client 
+                char buffer[BUFFER_SIZE]; 
+                memset(buffer, 0, BUFFER_SIZE);
 
-        printf("allowing another client..\n");
-        from_client2 = server_handshake(&to_client2);
-        printf("connected to another client\n");
-        char buffer[BUFFER_SIZE];
-        while (1) {
-            int bytesRead = read(from_client, buffer, sizeof(buffer));
-            if (bytesRead == 0) {
-                printf("a client disconnected\n");
-                break;
-            }
-            if (bytesRead < 0) {
-                perror("server fail to read message from client");
-                exit(1);
-            }
-            printf("bytes read: %d\n", bytesRead);
-            printf("Server received message : '%s'\n", buffer);
-            
+                int bytesRead = read(i, buffer, sizeof(buffer));
+                if (bytesRead < 0) {
+                    perror("server read error");
+                    exit(1);
+                }
+                printf("received from client '%s' (read %d bytes)\n", buffer, bytesRead);
 
-            // send to all clients
-            int bytesWritten = write(to_client, buffer, strlen(buffer) + 1);
-            if (bytesWritten < 0) {
-                perror("server fail to send message to client");
-                exit(1);
-            }
-            bytesWritten = write(to_client2, buffer, strlen(buffer) + 1);
-            if (bytesWritten < 0) {
-                perror("server fail to send message to client");
-                exit(1);
-            }
+                // do something (send message to other clients)
 
-            // read from client 2
-            bytesRead = read(from_client2, buffer, sizeof(buffer));
-            if (bytesRead == 0) {
-                printf("a client disconnected\n");
-                break;
-            }
-            if (bytesRead < 0) {
-                perror("server fail to read message from client");
-                exit(1);
-            }
-            printf("bytes read: %d\n", bytesRead);
-            printf("Server received message : '%s'\n", buffer);
-            
-            // send to all clients
-            bytesWritten = write(to_client, buffer, strlen(buffer) + 1);
-            if (bytesWritten < 0) {
-                perror("server fail to send message to client");
-                exit(1);
-            }
-            bytesWritten = write(to_client2, buffer, strlen(buffer) + 1);
-            if (bytesWritten < 0) {
-                perror("server fail to send message to client");
-                exit(1);
+                // done reading, remove socket from list of available file descriptors to read from
+                if (bytesRead == 0) {
+                    FD_CLR(i, &read_fds); 
+                }
             }
         }
-        close(from_client);
-        close(to_client);
-
-        close(from_client2);
-        close(to_client2);
     }
 }
